144dcd7196ea3d23681c2b42b83a5c167b677cdf
Andrei <aakanaev2016@mail.ru>
2023-11-25 13:04:16 +0300

    add method show cards

--- libs/players.py
+++ libs/players.py
@@ -1,6 +1,15 @@
 """Игрок"""
-
+from libs.cards import Card
 
 class Player:
     def __init__(self, name):
         self.name = name
+        self.player_cards = [] 
+
+    def show_cards(self):
+        """возвращает карты игрока"""
+        return self.player_cards
+    
+    def receive_cards(self, cards):
+        """Принимает карты и добавляет их к картам игрока"""
+        self.player_cards.extend(cards) 
